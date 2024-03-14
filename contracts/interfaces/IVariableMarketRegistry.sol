ea929a403b8db338fdfb2e7f3da31fcf12269a50
blockchain5992 <ayushporwal91@gmail.com>
2024-03-14 09:59:04 +0530

    feat: updated interface

--- contracts/interfaces/IVariableMarketRegistry.sol
+++ contracts/interfaces/IVariableMarketRegistry.sol
@@ -1,42 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.20;
 
-/**
- * @title IVariableMarketRegistry
- * @dev Interface for the VariableMarketRegistry contract.
- */
 interface IVariableMarketRegistry {
-    /**
-     * @dev Returns the address of the VariableVault contract.
-     * @return variableVault The address of the VariableVault contract.
-     */
-    function variableVault() external view returns (address);
+    function activePerpMarkets(bytes32) external view returns (bool);
 
-    /**
-     * @dev Returns the address of the VariableLedger contract for a given baseToken and quoteToken pair.
-     * @param baseToken The address of the baseToken in the trading pair.
-     * @param quoteToken The address of the quoteToken in the trading pair.
-     * @return margin The address of the VariableLedger contract.
-     */
-    function getPerpLedger(
-        address baseToken,
-        address quoteToken
-    ) external view returns (address margin);
-
-    /**
-     * @dev Updates the VariableVault address. Only callable by the owner.
-     * @param newVault The new address of the VariableVault contract.
-     */
     function updateVariableVault(address newVault) external;
 
-    /**
-     * @dev Creates a new VariableLedger contract for a given baseToken and quoteToken pair.
-     * @param baseToken The address of the baseToken in the trading pair.
-     * @param quoteToken The address of the quoteToken in the trading pair.
-     * @return margin The address of the newly created VariableLedger contract.
-     */
-    function createPerpLedger(
-        address baseToken,
-        address quoteToken
-    ) external returns (address margin);
+    function updateVariableController(address newController) external;
+
+    function registerPerpMarket(bytes32 perpMarketId) external;
+
+    function deRegisterPerpMarket(bytes32 perpMarketId) external;
 }
